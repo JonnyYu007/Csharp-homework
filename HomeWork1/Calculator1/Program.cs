123a59e01d7e98caf431f7ca5e28e817117ea60b
Jonny <3491315181@qq.com>
2021-03-06 23:28:32 +0800

    the first homework

---
+++ HomeWork1/Calculator1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = "";
+            double a, b;
+            double s = 0;
+            char c;
+            Console.Write("请输入操作数1:");
+            n = Console.ReadLine();
+            a = Double.Parse(n);
+            Console.Write("请输入操作数2:");
+            n = Console.ReadLine();
+            b = Double.Parse(n);
+            Console.Write("请选择运算符(+,-,*,/,%):");
+            c = (char)Console.Read();
+            switch (c)
+            {
+                case '+':
+                    s = a + b;
+                    break;
+                case '-':
+                    s = a - b;
+                    break;
+                case '*':
+                    s = a * b;
+                    break;
+                case '/':
+                    s = a / b;
+                    break;
+                case '%':
+                    s = a % b;
+                    break;
+                default:
+                    Console.WriteLine("您输入的运算符不正确！");            
+                    break;
+            }
+            Console.WriteLine("运算的结果是:" + s);
+            Console.WriteLine("谢谢使用！");
+        }
+    }
+}
---
+++ HomeWork1/Calculator2/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Calculator2";
+            textBox1.Text = "操作数1";
+            textBox2.Text = "操作数2";
+            comboBox1.Text = "选择运算符";
+            textBox3.Text = "运算结果";
+            label1.Text = "   =";
+            button1.Text = "运算按钮";
+            comboBox1.Items.Add("+");
+            comboBox1.Items.Add("-");
+            comboBox1.Items.Add("*");
+            comboBox1.Items.Add("/");
+            comboBox1.Items.Add("%");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double op1, op2;
+            double s = 0;
+            op1 = Convert.ToSingle(textBox1.Text);
+            op2 = Convert.ToSingle(textBox2.Text);
+            if(comboBox1.Text=="+")
+            {
+                s = op1 + op2;
+                textBox3.Text = s.ToString();
+            }
+            else if(comboBox1.Text=="-")
+            {
+                s = op1 - op2;
+                textBox3.Text = s.ToString();
+            }
+            else if(comboBox1.Text=="*")
+            {
+                s = op1 * op2;
+                textBox3.Text = s.ToString();
+            }
+            else if(comboBox1.Text=="/")
+            {
+                s = op1 / op2;
+                textBox3.Text = s.ToString();
+            }
+            else
+            {
+                s = op1 % op2;
+                textBox3.Text = s.ToString();
+            }
+
+        }
+    }
+}
