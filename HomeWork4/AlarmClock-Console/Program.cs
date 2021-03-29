5c2d23f3026fc0032418ee627e8aea05d13f3f06
Jonny <3491315181@qq.com>
2021-03-29 23:42:32 +0800

    第四次作业（1）

---
+++ HomeWork4/AlarmClock-Console/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Alarm
+{
+    class Clock
+    {
+        private int hour;
+        private int minute;
+        private int second;
+        public Clock(int hour = 0, int minute = 0, int second = 0)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+        public override bool Equals(object obj)
+        {
+            var time = obj as Clock;
+            return time != null;
+        }
+    }
+    class AlarmClock
+    {
+        public Clock CurrentTime { get; set; }
+        public Clock AlarmTime { get; set; }
+        public event Action<AlarmClock> AlarmEvent;
+        public AlarmClock{
+            CurrentTime=new Clock();
+            }
+        void run(int a,int b,int c)
+    {
+        AlarmClock.hour = CurrentTime.hour + a;
+        AlarmClock.minute = CurrentTime.minute + b;
+        AlarmClock.second = CurrentTime.second + c;
+        if (AlarmTime.Equals(CurrentTime))
+            AlarmEvent(this);
+        Thread.Sleep(1000);
+    }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        Console.WriteLine("实时时间："+DateTime.Now.ToString());
+        private static void ShowTime(AlarmClock sender)
+        {
+            ClockTime time = sender.CurrentTime;
+            Console.WriteLine($"AlarmEvent Event: " +
+              $"{time.hour}:{time.minute}:{time.second}");
+        }
+        
+        Console.WriteLine("请输入闹钟的时间:");
+        Console.Write("时:");
+        int a = Console.Read();
+        Console.Write("分:");
+        int b= Console.Read();
+        Console.Write("秒:");
+        int c = Console.Read();
+
+        AlarmClock alarm = new AlarmClock();
+        alarm.AlarmTime = new Clock();
+        clock.AlarmEvent += ShowTime;
+        alarm.run(a, b, c);
+    }
+    }
+}
---
+++ HomeWork4/GenericList/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace homework4
+{
+    public class Node<T>
+    {
+        public Node<T> Next;
+        public T Data;
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class Genericlist<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public Genericlist()
+        {
+            head = tail = null;
+        }
+        public Node<T> Head//属性
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node <T>n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> n = head; n != null; n = n.Next)
+            {
+                action(n.Data);
+            }
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Genericlist<int> list = new Genericlist<int>();
+            Random r = new Random();
+            for(int i = 0; i < 20; i++)
+            {
+                list.Add(r.Next(100));
+            }
+            Console.Write("遍历泛型链表的结果是:");
+            list.ForEach(n => Console.Write(n+" "));
+            double min = 100;
+            double max = 0;
+            double sum = 0;
+            list.ForEach(n => min = min > n ? n : min);
+            list.ForEach(n => max = max > n ? max : n);
+            list.ForEach(n => sum += n);
+            
+            Console.WriteLine("\n"+$"min={min},max={max},sum={sum}");
+        }
+    }
+}
+
